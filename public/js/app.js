e71b52ec0b13e493e006b7eb8ee57c0439e74561
Sammy Schneider <SammySchneider@Sammys-MacBook-Air.local>
2020-10-03 13:32:09 -0700

    messing around with edit route

--- public/js/app.js
+++ public/js/app.js
@@ -47,13 +47,14 @@ class App extends React.Component {
   // LOADS CRUD (REVIEW SCHEMA)
   createReview = (event) => {
     event.preventDefault()
-    axios.post('/foods', this.state.review).then(response => {
+    event.target.reset()
+    axios.post('/foods', this.state).then(response => {
       this.setState({
         reviews: response.data
       })
     })
   }
-// Deletes Review
+// DELETE REVIEW
 deleteReview = (event) => {
   axios.delete('/foods/' + event.target.id).then(response => {
     this.setState({
@@ -61,56 +62,84 @@ deleteReview = (event) => {
     })
   })
 }
+
+//EDIT REVIEW
+updateReview = (event) => {
+  event.preventDefault()
+  axios.put('/foods/'+ event.target.id, this.state.review).then(response => {
+    this.setState({
+      reviews: response.data
+    })
+  })
+}
+//TOGGLE FORM
+  toggleForm = (event) => {
+      this.setState({
+        show: !this.state.show
+      })
+  }
 render = () => {
   return(
     <div>
     <div className="city">
     <div className="find-button">
     <button onClick={this.findFood}>Find Restaurants</button>
-    <h3>City: {this.state.foods.city}</h3>
     </div>
     </div>
+    <div className="card-container">
     {this.state.restaurant.map (food => {
       return(
-        <div className="card-container">
           <div className="food-card">
             <img src={food.restaurant.featured_image} alt="food-pic"/>
           <div className="food-info">
-            <h3>Name: <a href={food.restaurant.url}>{food.restaurant.name}</a></h3>
-            <h3>Cuisines: {food.restaurant.cuisines}</h3>
-            <h3>location: {food.restaurant.location.address}</h3>
-            <h3>Reviews: </h3>
-            {
-              this.state.reviews.filter((review) => {
-                return food.restaurant.id == review.restaurant_id
-              })
-              .map((review, i) => {
-                return(
-                  <div key={i}>
-                    <h3>Name:{review.name}</h3>
-                    <p>Rating:{review.rating}</p>
-                    <p>{review.review_content}</p>
-                    <button onClick={this.deleteReview} id={review._id}>delete</button>
-                  </div>
-                )
-              })
-            }
-            <form onSubmit={this.createReview}>
+            <h3><a className='anchors' href={food.restaurant.url}>{food.restaurant.name}</a></h3>
+            <h5 className="weight">{food.restaurant.cuisines}</h5>
+            <h6 className="weight">{food.restaurant.location.address}</h6>
+            <hr className="hr-light my-4 wow fadeInDown" data-wow-delay="0.4s"/>
+            <button onClick={this.toggleForm} className="btn btn-light add-review"><i className="fas fa-plus"></i></button>
+            <h6 className="weight review-padding">Reviews: </h6>
+            {this.state.show ? <form onSubmit={this.createReview}>
               <input id='restaurant_id' type='hidden' value={food.restaurant.id} />
-              <label htmlFor="name">Name: </label>
-              <input id='name' type='text' onChange={this.handleChange} />
+              <input className='rating' id='name' type='text' placeholder="Name" onChange={this.handleChange} />
               <br/>
-              <label htmlFor="review">Review: </label>
-              <input id='review_content' type='text' onChange={this.handleChange} />
+              <textarea className='rating' id='review_content' placeholder="Review" onChange={this.handleChange} />
               <br/>
-              <label htmlFor="rating">Rating: </label>
-              <input id='rating' type='number' min='0' max='5' onChange={this.handleChange} />
-              <input type="submit" value="Add A Review" className="update-btn" />
-            </form>
+              <input className='rating' id='rating' type='number' min='0' max='5' placeholder="Rating" onChange={this.handleChange} />
+              <br/>
+              <input type="submit" value="Add Review" className="btn-sm btn-secondary" />
+            </form> : null}
+              {
+                this.state.reviews.filter((review) => {
+                  return food.restaurant.id == review.restaurant_id
+                })
+                .map((review, i) => {
+                  return(
+                    <div key={i} className='review-box'>
+                      <p>Name: {review.name}</p>
+                      <p>Rating: {review.rating}</p>
+                      <p>{review.review_content}</p>
+                      <button className='btn-sm btn-outline-danger' onClick={this.deleteReview} id={review._id}>Remove</button>
+                      <details>
+                        <summary>
+                          <i className="fas fa-pencil-alt"></i>
+                        </summary>
+                        <form onSubmit={this.updateReview} id={review._id}>
+                          <input type='text' id='name' className='rating' onChange={this.handleChange}/>
+                          <br/>
+                          <textarea className='rating' id='review_content' onChange={this.hangleChange}/>
+                          <br/>
+                          <input type='number' className='rating' id='rating' min='0' max='5'onChange={this.handleChange}/>
+                          <input className="btn-sm btn-outline-success" type="submit" value="Update" />
+                          </form>
+                      </details>
+                     </div>
+                  )
+                })
+              }
           </div>
           </div>
-        </div>
       )})}
+      </div>
     </div>
   )
   }
