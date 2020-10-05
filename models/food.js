442e20ca6681760af7c905a2185f258fb86c9dfe
Arjun Singh <arjunsingh@Arjuns-MacBook-Air.local>
2020-10-04 21:11:27 -0700

    .

--- models/food.js
+++ models/food.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose')
 
 const foodSchema = new mongoose.Schema ({
+  img: String,
+  restaurantName: String,
+  address: String,
+  cuisines: String,
+  ratings: {type: Number , max:5, min:0},
   name: String,
-  review_content: String,
+  review_content:String,
   rating: {type: Number , max:5, min:0},
-  restaurant_id: String //Added this key value so that we can associate the restuarant with it's review
+  restaurant_id: String
 })
 
 
-const Food = mongoose.model('Review', foodSchema)
+const Food = mongoose.model('Food', foodSchema)
 
 module.exports = Food;
