fc07a31cf52e2fe899708da4c6c91cd0b9dd7d0e
Ricky Shah <ricky@Rickys-MacBook-Pro.local>
2020-10-05 21:56:54 -0400

    footer

--- public/js/app.js
+++ public/js/app.js
@@ -71,15 +71,15 @@ updateRestaurant = (event) => {
         })
       })
     }
-  // LOADS CRUD (REVIEW SCHEMA)
- createReview = (event) => {
-   event.preventDefault()
-   axios.post('/foods', this.state.review).then(response => {
-     this.setState({
-       reviews: response.data
-     })
+// LOADS CRUD (REVIEW SCHEMA)
+createReview = (event) => {
+ event.preventDefault()
+ axios.post('/foods', this.state.review).then(response => {
+   this.setState({
+     reviews: response.data
    })
- }
+ })
+}
 
    // Deletes Review
 deleteReview = (event) => {
@@ -115,11 +115,11 @@ handleCityChange = (event) => {
   })
 }
 
-  //HOW TO SET THE CHANGES
-  handleChange = (event) => {
-    this.setState({
-        [event.target.id]: event.target.value
-    })
+//HOW TO SET THE CHANGES
+handleChange = (event) => {
+  this.setState({
+      [event.target.id]: event.target.value
+  })
 }
 //HOW TO SET THE CHANGES
 reviewChange = (event) => {
@@ -132,62 +132,62 @@ reviewChange = (event) => {
   })
 }
   //LOADS ZOMATO API DIRECTLY ON PAGE
-  findByName = (event) => {
-     event.preventDefault()
-     axios.get('https://developers.zomato.com/api/v2.1/location_details?apikey=a5408e7fd89832c5bc693f21db7f0abf&entity_id='+ this.state.city+'&entity_type=city').then(
-         (response) => {
-           this.setState({
-             foods: response.data,
-             restaurant: response.data.best_rated_restaurant,
-             cityName: response.data.city
-           })
-           // console.log(this.state.id);
-         }
-     )}
+findByName = (event) => {
+   event.preventDefault()
+   axios.get('https://developers.zomato.com/api/v2.1/location_details?apikey=a5408e7fd89832c5bc693f21db7f0abf&entity_id='+ this.state.city+'&entity_type=city').then(
+       (response) => {
+         this.setState({
+           foods: response.data,
+           restaurant: response.data.best_rated_restaurant,
+           cityName: response.data.city
+         })
+         // console.log(this.state.id);
+       }
+   )}
 
 newYork= (event) => {
-  event.preventDefault()
-    axios.get('https://developers.zomato.com/api/v2.1/location_details?apikey=a5408e7fd89832c5bc693f21db7f0abf&entity_id=280&entity_type=city').then(
-        (response) => {
-        this.setState({
-          foods: response.data,
-          restaurant: response.data.best_rated_restaurant,
-          })
-              // console.log(this.state.id);
-            }
-        )}
+event.preventDefault()
+  axios.get('https://developers.zomato.com/api/v2.1/location_details?apikey=a5408e7fd89832c5bc693f21db7f0abf&entity_id=280&entity_type=city').then(
+      (response) => {
+      this.setState({
+        foods: response.data,
+        restaurant: response.data.best_rated_restaurant,
+        })
+            // console.log(this.state.id);
+          }
+      )}
 
-  lasVegas= (event) => {
-    event.preventDefault()
-      axios.get('https://developers.zomato.com/api/v2.1/location_details?apikey=a5408e7fd89832c5bc693f21db7f0abf&entity_id=282&entity_type=city').then(
+lasVegas= (event) => {
+  event.preventDefault()
+    axios.get('https://developers.zomato.com/api/v2.1/location_details?apikey=a5408e7fd89832c5bc693f21db7f0abf&entity_id=282&entity_type=city').then(
+        (response) => {
+          this.setState({
+            foods: response.data,
+            restaurant: response.data.best_rated_restaurant
+          })
+        }
+    )}
+washingtonDc= (event) => {
+  event.preventDefault()
+      axios.get('https://developers.zomato.com/api/v2.1/location_details?apikey=a5408e7fd89832c5bc693f21db7f0abf&entity_id=283&entity_type=city').then(
           (response) => {
             this.setState({
               foods: response.data,
               restaurant: response.data.best_rated_restaurant
-            })
-          }
-      )}
-  washingtonDc= (event) => {
-    event.preventDefault()
-        axios.get('https://developers.zomato.com/api/v2.1/location_details?apikey=a5408e7fd89832c5bc693f21db7f0abf&entity_id=283&entity_type=city').then(
-            (response) => {
-              this.setState({
-                foods: response.data,
-                restaurant: response.data.best_rated_restaurant
-                })
-              }
-          )}
-
-  findAustin= (event) => {
-    event.preventDefault()
-      axios.get('https://developers.zomato.com/api/v2.1/location_details?apikey=a5408e7fd89832c5bc693f21db7f0abf&entity_id=278&entity_type=city').then(
-          (response) => {
-          this.setState({
-          foods: response.data,
-          restaurant: response.data.best_rated_restaurant
               })
             }
-          )}
+        )}
+
+findAustin= (event) => {
+  event.preventDefault()
+    axios.get('https://developers.zomato.com/api/v2.1/location_details?apikey=a5408e7fd89832c5bc693f21db7f0abf&entity_id=278&entity_type=city').then(
+        (response) => {
+        this.setState({
+        foods: response.data,
+        restaurant: response.data.best_rated_restaurant
+            })
+          }
+        )}
 findChicago= (event) => {
   event.preventDefault()
     axios.get('https://developers.zomato.com/api/v2.1/location_details?apikey=a5408e7fd89832c5bc693f21db7f0abf&entity_id=292&entity_type=city').then(
@@ -245,22 +245,18 @@ render = () => {
         <label htmlFor="ratings">Ratings</label><br/>
         <input onChange={this.handleChange} type="number" min="0" max="5"step="0.1"id="ratings" />
         <br />
-
         <input className="submit" type="submit" value="Add Favorite Restaurant" />
       </form>
       </div>
    </details>
-
-    <ul>
+   <ul>
     <h2>My favorites</h2>
      {this.state.favorite.map((fav) => {
        return(
          <div>
           { (fav.restaurantName) ?
          <div className="favRest">
-
          <img className="favImg"src={fav.img}/>
-
          <details>
          <summary>{fav.restaurantName}</summary>
          <h4>Cuisines: {fav.cuisines}</h4>
@@ -297,10 +293,9 @@ render = () => {
           </details>
          </div> : null}
          </div>
-       )
-     })}
+       )})}
      </ul>
-</div>
+     </div>
       <div className="findRestaurant">
         <div className="find-button">
         <details className="dropDown">
@@ -323,89 +318,88 @@ render = () => {
         </div>
         <div className="card-container">
 
-        {this.state.restaurant.map (food => {
-          return(
-            <div className="food-card">
-            <img className="apiImg"src={food.restaurant.thumb} alt="food-pic"/>
-            <div className="food-info">
-            <h3 >Name: <a className="name"href={food.restaurant.url}>{food.restaurant.name}</a></h3>
-            <h3>Cuisines: {food.restaurant.cuisines}</h3>
-            <h3>location: {food.restaurant.location.address}</h3>
-            <h3>Ratings: {food.restaurant.user_rating.aggregate_rating}</h3>
-            <h3>Reviews: </h3>
-              {
-                this.state.reviews.filter(review => {
-                  return food.restaurant.id == review.restaurant_id
-                })
-                .map((review, i) => {
-                  return(
-                    <div key={i}>
-                      <p>Name: {review.name}</p>
-                      <p>Rating: {review.rating}</p>
-                      <p>Review: {review.review_content}</p>
-                      <details>
-                      <summary>Edit review</summary>
-                      <form id={review._id} onSubmit={this.updateReview}>
-                      <label htmlFor="name">Name</label>
-                      <br />
-                      <input
-                        type="text"
-                        id="name"
-                        onChange={this.handleChange}
-                        placeholder={review.name}
-                      />
-                      <br />
-                      <label htmlFor="review_content">Review</label>
-                      <br />
-                      <input
-                        type="text"
-                        id="review_content"
-                        onChange={this.handleChange}
-                        placeholder={review.review_content}
-                      />
-                      <br />
-                      <label htmlFor="rating">rating</label>
-                      <br />
-                      <input
-                        type="number"
-                        id="rating"
-                        min="0" max="5" step="0.1"
-                        onChange={this.handleChange}
-                        placeholder={review.rating}
-                      />
-                      <br />
-                      <input className="submit" type="submit" value="Edit Review" />
-                    </form>
-                    <button onClick={this.deleteReview} id={review._id}>delete</button>
-                      </details>
-                    </div>
-                  )
-                })
-              }
-              <details>
-              <summary>Add Review</summary>
-              <form onSubmit={this.createReview}>
-                <input id='restaurant_id' type='hidden' value={food.restaurant.id} />
-                <label htmlFor="name">Name: </label>
-                <input id='name' type='text' onChange={this.reviewChange} />
-                <br/>
-                <label htmlFor="review">Review: </label>
-                <input id='review_content' type='text' onChange={this.reviewChange} />
-                <br/>
-                <label htmlFor="rating">Rating: </label>
-                <input id='rating' type='number' min='0' max='5' step="0.1" onChange={this.reviewChange} />
-                <input className="submit" type="submit" value="Add A Review" />
-              </form>
-              </details>
-            </div>
-            </div>
+    {this.state.restaurant.map (food => {
+      return(
+        <div className="food-card">
+        <img className="apiImg"src={food.restaurant.thumb} alt="food-pic"/>
+        <div className="food-info">
+        <h3 >Name: <a className="name"href={food.restaurant.url}>{food.restaurant.name}</a></h3>
+        <h3>Cuisines: {food.restaurant.cuisines}</h3>
+        <h3>location: {food.restaurant.location.address}</h3>
+        <h3>Ratings: {food.restaurant.user_rating.aggregate_rating}</h3>
+        <h3>Reviews: </h3>
+          {
+            this.state.reviews.filter(review => {
+              return food.restaurant.id == review.restaurant_id
+            })
+            .map((review, i) => {
+              return(
+                <div key={i}>
+                  <p>Name: {review.name}</p>
+                  <p>Rating: {review.rating}</p>
+                  <p>Review: {review.review_content}</p>
+                  <details>
+                  <summary>Edit review</summary>
+                  <form id={review._id} onSubmit={this.updateReview}>
+                  <label htmlFor="name">Name</label>
+                  <br />
+                  <input
+                    type="text"
+                    id="name"
+                    onChange={this.handleChange}
+                    placeholder={review.name}
+                  />
+                  <br />
+                  <label htmlFor="review_content">Review</label>
+                  <br />
+                  <input
+                    type="text"
+                    id="review_content"
+                    onChange={this.handleChange}
+                    placeholder={review.review_content}
+                  />
+                  <br />
+                  <label htmlFor="rating">rating</label>
+                  <br />
+                  <input
+                    type="number"
+                    id="rating"
+                    min="0" max="5" step="0.1"
+                    onChange={this.handleChange}
+                    placeholder={review.rating}
+                  />
+                  <br />
+                  <input className="submit" type="submit" value="Edit Review" />
+                </form>
+                <button onClick={this.deleteReview} id={review._id}>delete</button>
+                  </details>
+                </div>
+              )
+            })
+          }
+          <details>
+          <summary>Add Review</summary>
+          <form onSubmit={this.createReview}>
+            <input id='restaurant_id' type='hidden' value={food.restaurant.id} />
+            <label htmlFor="name">Name: </label>
+            <input id='name' type='text' onChange={this.reviewChange} />
+            <br/>
+            <label htmlFor="review">Review: </label>
+            <input id='review_content' type='text' onChange={this.reviewChange} />
+            <br/>
+            <label htmlFor="rating">Rating: </label>
+            <input id='rating' type='number' min='0' max='5' step="0.1" onChange={this.reviewChange} />
+            <input className="submit" type="submit" value="Add A Review" />
+          </form>
+          </details>
+        </div>
+        </div>
 )})}
-
+        </div>
       </div>
-</div>
+      
     </div>
-  )
-  }
+  )}
 }
 
 
